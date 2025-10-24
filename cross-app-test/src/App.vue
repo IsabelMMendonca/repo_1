c1280f3526ea535e39f3e8a06553103da729ac44
celebels <abcddefghijklmnopqrstuvwxyz1@gmail.com>
2025-10-24 13:59:06 -0300

    remove extra template to leave only component rendering

--- cross-app-test/src/App.vue
+++ cross-app-test/src/App.vue
@@ -3,14 +3,6 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="flex align-center justify-center m-4">
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
